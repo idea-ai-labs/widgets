e2eb710f5e4fa552d3fc161d4537bba666b40cd8
idea-ai-labs <idea.ai.labs@gmail.com>
2026-04-12 18:39:07 -0400

    Create typography.ts

---
+++ app/styles/typography.ts
@@ -0,0 +1,51 @@
+export const type = {
+  largeTitle: {
+    fontSize: 34,
+    fontWeight: 700,
+    letterSpacing: -0.8,
+    lineHeight: 1.1,
+  },
+
+  title1: {
+    fontSize: 28,
+    fontWeight: 700,
+    letterSpacing: -0.6,
+    lineHeight: 1.2,
+  },
+
+  title2: {
+    fontSize: 22,
+    fontWeight: 600,
+    letterSpacing: -0.4,
+    lineHeight: 1.25,
+  },
+
+  title3: {
+    fontSize: 18,
+    fontWeight: 600,
+    letterSpacing: -0.2,
+    lineHeight: 1.3,
+  },
+
+  body: {
+    fontSize: 16,
+    fontWeight: 400,
+    lineHeight: 1.5,
+  },
+
+  callout: {
+    fontSize: 15,
+    fontWeight: 400,
+    opacity: 0.85,
+  },
+
+  caption1: {
+    fontSize: 13,
+    opacity: 0.7,
+  },
+
+  caption2: {
+    fontSize: 11,
+    opacity: 0.6,
+  },
+};
